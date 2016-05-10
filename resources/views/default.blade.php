8161f545567bd9c8539bd4ac14e336b03c8ff1ba
Suresh <sureshkumardasari@gmail.com>
2016-05-10 19:08:08 +0530

    #tinymice editor

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -13,13 +13,15 @@
 
     <link href="{{ asset('/css/dataTables.bootstrap.min.css') }}" rel='stylesheet' type='text/css'>
 
-
+    @section('header-assets')
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	@show
 </head>
 <body>
 	@section('top-nav')
