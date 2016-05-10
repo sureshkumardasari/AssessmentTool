517d17a6dba704eeeba5831bd2a4e3dba7b1ef4e
Suresh <sureshkumardasari@gmail.com>
2016-05-10 12:05:24 +0530

    # Footer changes

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,9 @@
     <!-- FOOTER -->
 	<footer class="text-center">	
 		<div class="container">
-	        <p class="text-muted">{{ Inspiring::quote() }}</p>
+	        <!-- <p class="text-muted">{{ Inspiring::quote() }}</p> -->
 	    </div>	
-		<p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+		<p>&copy;2016 <a href="http://appstekcorp.com">AppsTek Corp</a>. All Rights Reserved. </p>
 		<span id="top-link-block" class="pull-right hidden">
 		    <a href="#top" class="well well-sm"  onclick="$('html,body').animate({scrollTop:0},'slow');return false;">
 		        <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
