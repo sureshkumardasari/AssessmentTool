edda4c50097e544670d58d9c6ffc8c8fc6827c6a
mallikharjun madamanchi <mmadamanchi@appstekcorp.com>
2016-07-01 12:49:48 +0530

    Passage preview in fancy box with a slight Error

---
+++ app/Modules/resources/Views/passage/main_popup.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: APPSTEK5
+ * Date: 30/06/2016
+ * Time: 18:13
+ */
---
+++ app/Modules/resources/Views/passage/popup.blade.php
@@ -0,0 +1,50 @@
+      <div id="popup_data">
+      <div class="form-group ">
+       Institution:
+       
+        {{$institution['name']}}
+        </select>
+       </div>
+      </div>
+      <div class="form-group ">
+       Category:
+       
+       {{$category['name']}}
+       
+      </div>
+      <div class="form-group ">
+       Subject:
+      
+        {{$subject['name']}}
+       
+      </div>
+      <div class="form-group ">
+       Lessons:
+        {{$lessons['name']}}
+       
+      </div>
+      
+
+
+      <div class="form-group ">
+       Passage Title:
+       
+       {{ $passage['passagetitle'] }}
+       </div>
+      </div>
+      <div class="form-group ">
+       Passage Text:
+       {{ $passage['passagetext']}}
+       
+      </div>
+       <div class="form-group ">
+      Passage Lines:
+       {{ $passage['passagelines']}}
+       
+      </div>
+      
+      </div>
+      
+       </div>
+      </div>
+
