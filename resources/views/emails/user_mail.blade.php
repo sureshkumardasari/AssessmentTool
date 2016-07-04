e0222b8432b1f09f0cc405aedc456f8e60d3520e
Raghuvendrareddy <rguvvala@appstekcorp.com>
2016-07-04 16:02:14 +0530

    Email Send after registeration

---
+++ resources/views/emails/user_mail.blade.php
@@ -0,0 +1,71 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ </head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="top" bgcolor="#000" style="background-color:#000;">
+    
+    <table width="600" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="70" align="left" valign="middle"></td>
+      </tr>
+      
+      <tr>
+        <td align="left" valign="top" bgcolor="#564319" style="background-color:#AEB7BD; font-family:Arial, Helvetica, sans-serif; padding:10px;margin-left:20px"><div style="font-size:36px; color:#ffffff;"><img src="http://www.appstekcorp.com/wp-content/uploads/2015/09/AppsTek-Corp-logo-200x89.png" width="200" height="89" style="display:block;"></div>
+      
+          </td>
+      </tr>
+      <tr>
+        <td align="left" valign="top" bgcolor="#ffffff" style="background-color:#ffffff;"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" valign="middle" style="padding:10px; color:#564319; font-size:22px; font-family:Georgia, 'Times New Roman', Times, serif;">Thank you for registered on Assessment Tool</td>
+            
+          </tr>
+         </table>
+        
+         
+          <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" style="margin-bottom:15px;">
+            <tr>
+              
+            </tr>
+          </table>
+          <table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-bottom:10px;">
+            <tr>
+              
+            </tr>
+          </table>
+          <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0">
+            <tr>
+              <td width="50%" align="left" valign="middle" style="padding:10px;"><table width="75%" border="0" cellspacing="0" cellpadding="4">
+                <tr>
+                 <td> Regards,</td></tr>
+               <tr><td> Support Team</td></tr>
+            
+
+
+                </tr>
+                <tr>
+                  <td align="left" valign="top" style="font-family:Verdana, Geneva, sans-serif; font-size:12px; color:#000000;"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      
+                    </tr>
+                  </table></td>
+                </tr>
+              </table></td>
+              
+            </tr>
+          </table></td>
+      </tr>
+      <tr>
+       
+      </tr>
+      </table>
+    <br>
+    <br></td>
+  </tr>
+</table>
+</body>
+</html>
