940fa7b7f84cb530efe0af4163a118da03111e16
somu <spamidi@appstekcorp.com>
2016-07-27 15:07:12 +0530

    Whole Class Score Report fixed

--- app/Modules/report/Views/report/student_answer_report_list.blade.php
+++ app/Modules/report/Views/report/student_answer_report_list.blade.php
@@ -23,7 +23,7 @@ $arr=[1=>'A',2=>'B',3=>'C',4=>'D',5=>'E'];
         @if($assignment->is_correct=="No")
            <p style="color:red;font:bold;">{{$assignment->your_answer}}&nbsp;&nbsp;<span class="glyphicon glyphicon-remove"></span></p> 
         @else
-            <p style="color:green;font:bold;"><b>{{$assignment->your_answer}} &nbsp;&nbsp;<span class="glyphicon glyphicon-ok"></span></p> 
+            <p style="color:green;font:bold;"><b>{{$assignment->your_answer}} &nbsp;&nbsp;<span class="glyphicon glyphicon-ok"></span></b></p>
         @endif
         </td>
     </tr>
