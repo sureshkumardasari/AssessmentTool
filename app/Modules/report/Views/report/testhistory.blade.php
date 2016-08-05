250d57237b34b158898e48021bb46d705d4605d0
siva <schintalapudi@appstekcorp.com>
2016-08-05 19:29:49 +0530

    Test history tile done

--- app/Modules/report/Views/report/testhistory.blade.php
+++ app/Modules/report/Views/report/testhistory.blade.php
@@ -27,3 +27,55 @@
 </table>
 
 
+
+<div id="chart-1">FusionCharts XT will load here!</div>
+<script src="{{ asset('/js/fusion/js/fusioncharts.js') }}"></script>
+<script type="text/javascript" src="{{asset('/js/fusion/js/themes/fusioncharts.theme.ocean.js')}}"></script>
+
+<script>
+
+    FusionCharts.ready(function(){
+        var salesChart = new FusionCharts({
+            type: 'column2d',
+            dataFormat: 'json',
+            renderAt: 'chart-1',
+            width: '550',
+            height: '350',
+            dataSource: {
+                "chart": {
+                    "caption": "Test History Report",
+                    "subCaption": "",
+                    "xAxisName": "Assignment Name",
+                    "yAxisName": "Total Students",
+                    "numberPrefix": "",
+                    "theme": "fint"
+                },
+                // "categories": [
+                //     {
+                //         "category": [
+                //          @foreach($assignments as $user_id => $assignment)
+                //          {
+                //             'label' : '{{$assignment->assign_name}}'
+                //         },
+                //         @endforeach
+                //     ]
+                //     }
+                // ],
+                // "dataset": [
+                //     {
+                        "data": [
+                                @foreach($assignments as $user_id => $assignment)
+                                {
+                                    'label' : '{{$assignment->assign_name}}',
+                                'value' : '{{$marks[$assignment->assign_id]}}%'
+                            },
+                            @endforeach
+                    // ]
+                    // }
+                ]
+            }
+        }).render();
+    });
+</script>
+
+
