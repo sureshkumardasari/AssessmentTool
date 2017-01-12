9f310b0d4cbfd581d8592e92ac2a12ca23a976b0
Sirisha <srevu@appstekcorp.com>
2017-01-12 16:26:56 +0530

    footer year change

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
     <!-- FOOTER -->
 	<footer class="text-center footer">	
-		<p>&copy;2016 <a href="http://appstekcorp.com"><img class="logo-img" src="{{ asset('/images/AppsTek-Corp-logoH.png') }}" width="120" title="AppsTek Corp" alt="AppsTek Corp"></a>. All Rights Reserved. 
+		<p>&copy;<?php echo date('Y');?> <a href="http://appstekcorp.com"><img class="logo-img" src="{{ asset('/images/AppsTek-Corp-logoH.png') }}" width="120" title="AppsTek Corp" alt="AppsTek Corp"></a>. All Rights Reserved. 
 		<!-- <span id="top-link-block" class="pull-right hidden">
 		    <a href="#top" class="well well-sm"  onclick="$('html,body').animate({scrollTop:0},'slow');return false;">
 		        <i class="glyphicon glyphicon-chevron-up"></i> Back to Top
