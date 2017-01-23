38c6f0cad0186e644819d25e0352dde4109a76cb
Bhavana <bmulinti@appstekcorp.com>
2017-01-23 18:16:38 +0530

    Revert "#335 resolved"
    This reverts commit 95c38c16b4f88b6ac0bae8f5b61668328ea11e17.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -117,7 +117,6 @@
 								<div class="form-group ">
 									<div class="col-md-6 col-md-offset-4">
 										<button type="submit" class="btn btn-primary" onclick="setTimeout(myFunction, 3000);">Register</button>
-										<a type="Cancel"  class="btn btn-danger"  href="{{  url('/auth/login/') }}">Cancel</a>
 									</div>
 								</div>
 							</div>
