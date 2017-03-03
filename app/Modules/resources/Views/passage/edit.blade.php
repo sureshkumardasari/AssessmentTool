e7b34acd560019521127f5becbc4cc468e29a862
Thirumala <tabburi@appstekcorp.com>
2017-03-02 15:58:31 -1200

    #858

--- app/Modules/resources/Views/passage/edit.blade.php
+++ app/Modules/resources/Views/passage/edit.blade.php
@@ -105,8 +105,8 @@
       <div class="form-group required">
        <label class="col-md-2 control-label">Status</label>
        <div class="col-md-6">
-        <input type="radio" class="" name="status" id="status_yes" value="1" {{ ($passage->status == 1 ) ? 'checked="checked"' : '' }}> Active 
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" class="" name="status" id="status_no" value="0" {{ ($passage->status == 0) ? 'checked="checked"' : '' }}> Inactive 
+        <input type="radio" class="" name="status" id="status_yes" value="1" checked="checked"> Active
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" class="" name="status" id="status_no" value="0"> Inactive
        </div>
       </div>
       <div class="form-group">
