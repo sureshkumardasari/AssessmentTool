ff034e7ea7f9d0e0a44994edbbbb33e68827d33e
Bhavana <bmulinti@appstekcorp.com>
2017-03-09 14:38:22 +0530

    #875 modified...

--- app/Modules/admin/Views/branding/brand.blade.php
+++ app/Modules/admin/Views/branding/brand.blade.php
@@ -2,9 +2,7 @@
 @section('content')
 
 	<div class="container">
-	@if(Session::has('flash_message'))
-            <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! Session('flash_message') !!}</em></div>
-        @endif
+	
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1"> 
 				<div class="panel panel-default">
