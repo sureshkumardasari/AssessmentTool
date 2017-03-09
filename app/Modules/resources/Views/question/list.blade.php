42bb6284678e5750a16da75a832b60c60f071f22
Thirumala <tabburi@appstekcorp.com>
2017-03-08 15:31:25 -1200

    modifi in resource module

--- app/Modules/resources/Views/question/list.blade.php
+++ app/Modules/resources/Views/question/list.blade.php
@@ -38,8 +38,8 @@
 	
 					<div class="panel-body searchfilter-body hide">
 
-					<div class="form-group col-md-6">
-						<label class="col-md-2 control-label" >Institution</label>
+					<div class="form-group col-md-6 required">
+						<label class="col-md-2 control-label">Institution</label>
 						<div class="col-md-10">
 							<select class="form-control" name="institution_id" id="institution_id" onchange="change_institution()">
 								<option value="0">--Select Institution--</option>
@@ -49,7 +49,7 @@
 							</select>
 						</div>
 					</div>
-					<div class="form-group col-md-6">
+					<div class="form-group col-md-6 required">
 						<label class="col-md-2 control-label">Category</label>
 						<div class="col-md-10">
 							<select class="form-control" name="category_id" id="category_id" onchange="change_category()">
@@ -58,7 +58,7 @@
 							</select>
 						</div>
 					</div>
-					<div class="form-group col-md-6">
+					<div class="form-group col-md-6 required">
 						<label class="col-md-2 control-label">Subject</label>
 						<div class="col-md-10">
 							<select class="form-control" name="subject_id" id="subject_id" onchange="change_lessons()">
@@ -67,7 +67,7 @@
 							</select>
 						</div>
 					</div>
-					<div class="form-group col-md-6">
+					<div class="form-group col-md-6 required">
 						<label class="col-md-2 control-label">Lessons</label>
 						<div class="col-md-10">
 							<select class="form-control" name="lessons_id" id="lessons_id" onchange="change_question_type()">
