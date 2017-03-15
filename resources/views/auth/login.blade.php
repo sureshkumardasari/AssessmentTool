95e5834db3f653d4885bc9009241caf49b986360
Thirumala <tabburi@appstekcorp.com>
2017-03-14 17:24:06 -1200

    #927

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,14 +21,14 @@
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
+						<div class="form-group required">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
 							</div>
 						</div>
 
-						<div class="form-group">
+						<div class="form-group required">
 							<label class="col-md-4 control-label">Password</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -27,7 +27,7 @@
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
+						<div class="form-group required">
 							<label class="col-md-4 control-label">E-Mail Address</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
