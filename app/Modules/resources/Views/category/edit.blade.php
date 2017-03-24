3d09221ac5dabb978796436daaca4b388e0f822e
Bhavana <bmulinti@appstekcorp.com>
2017-03-24 14:42:48 +0530

    #1067 done

--- app/Modules/resources/Views/category/edit.blade.php
+++ app/Modules/resources/Views/category/edit.blade.php
@@ -9,7 +9,7 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Category Details</div>
+				<div class="panel-heading">Update Category</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
