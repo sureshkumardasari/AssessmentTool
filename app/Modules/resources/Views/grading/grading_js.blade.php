d7a3492f272a362ac771822edcde04be14803b4f
sivakrishna <schintalapudi@appstekcorp.com>
2017-04-05 15:29:47 +0530

    updated grade js

--- app/Modules/resources/Views/grading/grading_js.blade.php
+++ app/Modules/resources/Views/grading/grading_js.blade.php
@@ -27,15 +27,16 @@ $path = url()."/grading/";
 					url:'{{$path}}list-student-ajax/'+$('#user').val(),
 					type:'post',
 					success:function(response){
+						$('#assignmentstable').dataTable().fnDestroy();
  						$('.student_list').empty();
 						var tr;
 						for (var i = 0; i < response.length; i++) {
 							tr = $('<tr/>');
  							tr.append("<td>" + response[i].first_name +'\t'+ response[i].last_name + "</td>");
-  							tr.append("<td></td>");
   							tr.append("<td><a href=''><i class='icons ico-grade'></i></a></td>");
  							$('.student_list').append(tr);
 						}
+						$('#assignmentstable').dataTable();
  					}
 				}
 		)
