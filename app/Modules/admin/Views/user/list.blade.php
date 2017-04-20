4dc08a6c1cdf276c7f702ba5f2503d638c7f0691
Thirumala Abburi <tabburi@appstekcorp.com>
2017-04-20 17:59:04 +0530

    1330

--- app/Modules/admin/Views/user/list.blade.php
+++ app/Modules/admin/Views/user/list.blade.php
@@ -18,15 +18,13 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<input type="hidden" name="_token" class="hidden-token" value="{{csrf_token()}}">
-								
 				<div class="panel-heading">Users
-
 				@if (Auth::user()->id == 1)
 					<a href="{{ url('/user/add/') }}" class="btn btn-primary btn-sm right"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add </a>
 					<a href="{{ route('userBulkUpload') }}" class="btn btn-primary btn-sm right fancybox fancybox.ajax"><span class="glyphicon glyphicon-upload" aria-hidden="true"></span> Bulk Upload </a>
-					<a href="{{ URL::to('user/download/'.$institution_id) }}" class="btn btn-primary btn-sm right fancybox fancybox.ajax">Download XLS</a>
+					<a href="{{ URL::to('user/download') }}" class="btn btn-primary btn-sm right fancybox fancybox.ajax">Download XLS</a>
 					@else
-					<a href="{{ URL::to('user/download/'.$institution_id) }}" class="btn btn-primary btn-sm right fancybox fancybox.ajax">Download XLS</a>
+					<a href="{{ URL::to('user/download') }}" class="btn btn-primary btn-sm right fancybox fancybox.ajax">Download XLS</a>
 					@endif
 				</div>
 				
@@ -38,9 +36,12 @@
 						</div>
                          
 						<div class="panel-body searchfilter-body hide">
+						<form class="form-inline" role="form">
+						<input type="hidden" name="page" id="page" value="role_id">
 							<div class="form-group col-md-6  required">
 								<label class="col-md-6 control-label">Select Institution</label>
 								<div class="col-md-6">
+								<input type="hidden" name="page" id="page" value="role_id">
 									<select class="form-control" name="institution_id" id="institution_id">
 										<option value=0>All</option>
 										@foreach($inst_arr as $id=>$val)
@@ -62,11 +63,20 @@
 										@endforeach
 									</select>
 								</div>
+								<div class="form-group col-md-12">
+								<div class="col-md-6 col-md-offset-6">
+									<button type="button" class="btn btn-primary" onclick="filter();" >
+										Go
+									</button>
+								</div>
+							</div>
+							<div class="form-group" id="loadingdiv"></div>	
+							</form>	
 							</div>
 						</div>
 					</div>
 					@endif
-					<div id="user-list"> {!! $usersList !!} </div>					
+					<div id="user-list" style="min-height:50px;"> {!! $usersList !!} </div>					
 				</div>
 			</div>
 		</div>
@@ -82,7 +92,43 @@
          var csrf=$('Input#csrf_token').val();
          $('#flash').fadeOut();
      }, 5000);
- })
+ });
+
+     function filter(){ 
+      
+  if($('#institution_id').val() == 0 || ($('#role_id').val() == 0)){
+                alert("please select all the fields");
+    }
+            else{
+     var institution_id = $('#institution_id').val();
+     var role_id = $('#role_id').val();
+       console.log( "ready!" ); 
+
+     
+      $targetList = $('div#user-list'); 
+   
+     
+     var data = $( "#institution_id, #role_id").serializeArray();
+     //console.log('selecte instId ' + instId);
+     $targetList.html('');
+     $("#loadingdiv").html('<img src="/images/fancybox_loading.gif" border="0"> Loading...');
+     $.ajax({
+            type: "GET",
+            data: data,
+            url: userSearchRoute,
+            success: function (data) {
+                if (data.toLowerCase().indexOf("code") >= 0 && data.toLowerCase().indexOf("401") >= 0) {
+                    window.location.reload();
+                    return false;
+                }
+                if(data != ''){             
+                    $targetList.html(data);
+                }
+                $("#loadingdiv").html('');
+            }
+        });
+     }
+ };
  </script>
  {!! HTML::script(asset('/js/custom/users.js')) !!}
 @endsection
--- public/js/custom/users.js
+++ public/js/custom/users.js
@@ -1,25 +1,43 @@
 $( document ).ready(function() {
-    console.log( "ready!" );    
-    $('#institution_id,#role_id').on("change",function(){
-    	var instId = $(this).val();
-    	$targetList = $('div#user-list');	
-    	var data = $( "#institution_id,#role_id" ).serializeArray();
-    	//console.log('selecte instId ' + instId);
-    	$.ajax({
-			type: "GET",
-			data: data,
-			url: userSearchRoute,
-			success: function (data) {
-				if (data.toLowerCase().indexOf("code") >= 0 && data.toLowerCase().indexOf("401") >= 0) {
-					window.location.reload();
-					return false;
-				}
-				if(data != ''){				
-					$targetList.html(data);
-				}
-			}
-		});
-    });
+
+     function filter(){ 
+      // alert("safsdg"); 
+  if($('#institution_id').val() == 0 || ($('#role_id').val() == 0)){
+                alert("please select all the fields");
+    }
+            else{
+     var institution_id = $('#institution_id').val();
+     var role_id = $('#role_id').val();
+       console.log( "ready!" ); 
+
+     if(page == 'role_id')
+     {
+      $targetList = $('div#user-list'); 
+     }
+     
+     
+     var data = $( "#institution_id, #role_id").serializeArray();
+     //console.log('selecte instId ' + instId);
+     $targetList.html('');
+     $("#loadingdiv").html('<img src="/images/fancybox_loading.gif" border="0"> Loading...');
+     $.ajax({
+            type: "GET",
+            data: data,
+            url: userSearchRoute,
+            success: function (data) {
+                if (data.toLowerCase().indexOf("code") >= 0 && data.toLowerCase().indexOf("401") >= 0) {
+                    window.location.reload();
+                    return false;
+                }
+                if(data != ''){             
+                    $targetList.html(data);
+                }
+                $("#loadingdiv").html('');
+            }
+        });
+     }
+ };
+  
     $('.searchfilter').on("click",function(e){ 
     $('#institution_id').val('0');
     $('#role_id').val('0');
