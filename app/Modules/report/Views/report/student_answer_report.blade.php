9c5cc56e61dd001c5254e23e4f3dd5152fc50d91
Thirumala Abburi <tabburi@appstekcorp.com>
2017-04-20 15:18:12 +0530

    #1327 updated

--- app/Modules/report/Views/report/student_answer_report.blade.php
+++ app/Modules/report/Views/report/student_answer_report.blade.php
@@ -161,7 +161,28 @@
             )
 
         }
-       
+       $('#institution_id').on('change',function(){
+            var csrf=$('Input#csrf_token').val();
+            $.ajax(
+                    {
+
+                        headers: {"X-CSRF-Token": csrf},
+                        url:loadurl+ $('#institution_id').val(),
+                        type: 'post',
+                        success: function (response) {
+                            $('#report').empty();
+                            var a = response.length;
+                            $('#assignment_id').empty();
+                            var opt = new Option('--Select Assignment--', '0');
+                            $('#assignment_id').append(opt);
+                            for (i = 0; i < a; i++) {
+                                var opt = new Option(response[i].name, response[i].id);
+                                $('#assignment_id').append(opt);
+                            }
+                        }
+                    }
+            )
+        });
         /* function reports(){
             
                 $.ajax(
