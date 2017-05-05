b3a173a79f8955ee3a2df40873d3371a08b81f8a
rathnapujitha <rthondepu@appstekcorp.com>
2017-05-05 18:29:12 +0530

    responsive update

--- app/Modules/report/Views/report/report.blade.php
+++ app/Modules/report/Views/report/report.blade.php
@@ -9,39 +9,39 @@
                     </div>
 
                     <div class="panel-body">
-                        <div class="col-md-2" ></div>
-                        <div class="rcorners1 col-md-6" >
+                        <div class="col-md-2 col-sm-1" ></div>
+                        <div class="rcorners1 col-md-6 col-sm-6" >
                             <a href="{{ url('/report/class_average_and_student_scores_report') }}">Class Averages And Student Scores Report</a>
                         </div>
 
-                        <div class="col-md-3" ></div>
-                        <div class="rcorners1 col-md-6" >
+                        <div class="col-md-3 col-sm-8" ></div>
+                        <div class="rcorners1 col-md-6 col-sm-4" >
                             <a href="{{ url('/report/wholeclass') }}">Whole Class Score Report</a>
                         </div><br>
                     </div>
                     <div class="panel-body">
                         <div class="col-md-2" ></div>
-                        {{--<div class="rcorners1  col-md-6" >--}}
+                        {{--<div class="rcorners1  col-md-6 " >--}}
                             {{--<a href="{{ url('/report/student') }}">Scores By Students</a>--}}
                         {{--</div>--}}
-                        <div class="col-md-3" ></div>
-                        <div class="rcorners1  col-md-6" >
+                        <div class="col-md-3 col-sm-1" ></div>
+                        <div class="rcorners1  col-md-6 col-sm-6" >
                             <a href="{{ url('/report/answer') }}">Questions & Answers</a>
                         </div>
                     </div>
                     <div class="panel-body">
-                        <div class="col-md-2" ></div>
-                        <div class="rcorners1  col-md-6" >
+                        <div class="col-md-2 col-sm-1" ></div>
+                        <div class="rcorners1  col-md-6 col-sm-6" >
                             <a href="{{ url('/report/test-history') }}">Test History Class Averages</a>
                         </div>
-                        <div class="col-md-3" ></div>
-                        <div class="rcorners1  col-md-6" >
+                        <div class="col-md-3 col-sm-8" ></div>
+                        <div class="rcorners1  col-md-6 col-sm-4" >
                             <a href="{{ url('/report/student-answer-report') }}">Student Answer Report</a>
                         </div>
                     </div>
                     <div class="panel-body">
-                    <div class="col-md-5" ></div>
-                    <div class="rcorners1 col-md-6" >
+                    <div class="col-md-5 col-sm-1" ></div>
+                    <div class="rcorners1 col-md-6 col-sm-6" >
                         <a href="{{ url('/report/leastscore') }}">Least Scores Report</a>
                     </div><br>
                 </div>
