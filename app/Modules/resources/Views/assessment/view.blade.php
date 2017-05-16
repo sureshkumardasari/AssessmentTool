120bd9c58a1f2d1d243494399a8ad227cec69019
Bhavana <bmulinti@appstekcorp.com>
2017-05-16 12:48:17 +0530

    #1476 done

--- app/Modules/resources/Views/assessment/view.blade.php
+++ app/Modules/resources/Views/assessment/view.blade.php
@@ -19,16 +19,19 @@
                         <?php
                             $i=0;
                             $a=0;
-                            $b=0;
+                            $b=1;
 
-                        $ans_arr = ['A', 'B', 'C', 'D', 'E'];
-                            $num_arr=['1','2','3','4','5','6','7','8','9','10'];
+
+                        $ans_arr = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+                        /*$num_arr=['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];*/
+                        //dd($assessments);
                         ?>
                         <div class="row">
                             <label class="col-md-3 control-label"><b> Questions List:</b> </label>
                         </div>
 
                         @foreach($assessments as $passage_id => $questionsdata)
+
                             @if($passage_id > 0)
                                     <div class="row">
                                         <label class="col-md-3 control-label"><b>Passage Title:</b> </label>
@@ -39,10 +42,22 @@
                                         <div class="col-md-6">{!! strip_tags(htmlspecialchars_decode($questionsdata['psg_txt'])) !!}</div>
                                     </div>
                                 @endif
+                                <?php
+                                  
+                                $num_arr = [];
+                                for($n=0; $n<count($questionsdata['questions'] ); $n++){
+                                    $num_arr[$n+1] = $n+1;
+                                }
+                                //dd(count($questionsdata['questions']) );
+                               // print_r($num_arr);
+                                ?>
                         @foreach($questionsdata['questions'] as $questions)
-                            <?php $i=0;?>
+                            <?php $i=0;
+                             
+                            ?>
                         <div class="row">
-                            <label class="col-md-3 control-label"><b><b>{{$num_arr[$b++]}}</b>.Question Title:</b> </label>
+                            <label class="col-md-3 control-label"><b><b>
+                            {{$b++}}<b>.Question Title:</b> </label>
                             <div class="col-md-6">{{$questions['title']}}</div>
                         </div>
                         <div class="row">
