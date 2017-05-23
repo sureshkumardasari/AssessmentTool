3e19cd64853f8f86692543737ca8076a421aca20
Thirumala Abburi <tabburi@appstekcorp.com>
2017-05-23 14:00:56 +0530

    image image

--- resources/views/top-nav.blade.php
+++ resources/views/top-nav.blade.php
@@ -105,7 +105,8 @@ $session=Session::get('starttestT');
 					<!-- <li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img width="50" height="50" alt="{{ Auth::user()->name }}" src="{{ $profile_picture }}" class="img-circle"> {{ ucwords(Auth::user()->name) }} <span class="caret"></span></a> -->
                     <li class="dropdown">
-						<a href="#" class="dropdown-toggle textname" data-toggle="dropdown" role="button" aria-expanded="false"><img width="35" height="35" alt="{{ Auth::user()->name }}" src="{{ $profile_picture }}" class="img-circle"> {{ ucwords(Auth::user()->name) }} <span class="caret"></span></a>
+						<a href="#" class="dropdown-toggle textname" data-toggle="dropdown" role="button" aria-expanded="false" title="{{ ucwords(Auth::user()->name) }}" data-toggle="tooltip"><img width="35" height="35" alt="{{ Auth::user()->name }}" src="{{ $profile_picture }}" class="img-circle"> 
+						{{ ucwords(Auth::user()->name) }} <span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 					
 						<li><a href="{{ url('/user/profile') }}">Profile</a></li>
@@ -167,7 +168,7 @@ $session=Session::get('starttestT');
 					}
 					?>
                     <li class="dropdown">
-						<a href="#" class="dropdown-toggle textname" data-toggle="dropdown" role="button" aria-expanded="false"><img width="35" height="35" alt="{{ Auth::user()->name }}" src="{{ $profile_picture }}" class="img-circle"> {{ ucwords(Auth::user()->name) }} <span class="caret"></span></a>
+						<a href="#" class="dropdown-toggle textname" data-toggle="dropdown" role="button" aria-expanded="false" title="{{ ucwords(Auth::user()->name) }}" data-toggle="tooltip"><img width="35" height="35" alt="{{ Auth::user()->name }}" src="{{ $profile_picture }}" class="img-circle" > {{ ucwords(Auth::user()->name) }} <span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 					@if($session == 1 )
 					<input type="hidden" id="alert" name="alert" value="">
@@ -186,3 +187,9 @@ $session=Session::get('starttestT');
 			@endif
 		</div>
 	</nav>
+	
+	<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
