8bf2961216dd3dc02789e6fb634d93c8903f05b2
Bhavana <bmulinti@appstekcorp.com>
2017-05-29 11:35:21 +0530

    #branding fade in & out

--- app/Modules/admin/Views/branding/brand.blade.php
+++ app/Modules/admin/Views/branding/brand.blade.php
@@ -8,7 +8,7 @@
 			<div class="col-md-10 col-md-offset-1"> 
 				<div class="panel panel-default">
 					<div class="panel-heading">Branding</div>
-					<div class="userSuccMSG"></div>
+					
 					<div class="panel-body">
 						@if (count($errors) > 0)
 							<div class="alert alert-danger">
